6b1d17709fdd1c946bc5f4a964199da99ca4051d
Vusal <vusal@MacBook-Pro.local>
2020-03-19 01:58:38 -0700

    itertools module

---
+++ modules/iter_m.py
@@ -0,0 +1,17 @@
+import itertools as iter
+
+
+# num1 = [1, 2, 3, 4]; num2 = [5, 6, 7, 8]
+
+# for num in iter.chain(num1, num2):
+#     print(num) #output --> 1 2 3 4 5 6 7 8
+
+
+# toppings = ( "Peanut butter", "Mayonez", "Tomato", "Lettuce", "Ketchup", "Jelly" )
+
+# for (a, b, c, d) in iter.combinations(toppings, 4):
+#     print("Would you like {0}, {1}, {2} and {3} sandwich".format(a, b, c, d))
+#     #output all possible combinations with 4 toppings
+   
+
+

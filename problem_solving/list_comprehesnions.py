c96a33ad85c9b446202c373086e5cee57d7528d9
Vusal <vusal@MacBook-Pro.local>
2020-03-19 19:58:02 -0700

    list comprehensions

--- problem_solving/list_comprehesnions.py
+++ problem_solving/list_comprehesnions.py
@@ -1,10 +1,11 @@
 #print a list of all possible coordinates given by (i, j, k) on a 3D grid 
 #where the sum of (i + j + k) is not equal to N
 
-# i = 1; x = 2
-# j = 2; y = 3
-# k = 0; z = 1
+# x = 1; 
+# y = 2; 
+# z = 0; 
 # n = 3
+
 x, y, z, n = (int(input()) for _ in range(4))
 print([ [i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range( z + 1) if ( i + j + k != n)])
 

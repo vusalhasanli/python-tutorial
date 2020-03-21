c86cda36dbe4afd80a7e353e054f906d9a3e6015
Vusal <vusal@MacBook-Pro.local>
2020-03-20 21:36:21 -0700

    get data using API requests and convert it to python data structure

--- projects/calc_dist/get_data.py
+++ projects/calc_dist/get_data.py
@@ -2,9 +2,10 @@ import requests
 import json
 
 
-meteor_response = requests.get('https://data.nasa.gov/resource/gh4g-9sfh.json')
-meteor_data = meteor_response.json()
-# print(meteor_response.status_code)
+api_response = requests.get('https://data.nasa.gov/resource/gh4g-9sfh.json')
+print(api_response.status_code)
+api_data = api_response.json()
+# print(api_response.status_code)
 
 with open('meteor_data.json', 'w') as output_file:
-    json.dump(meteor_data, output_file)
+    json.dump(api_data, output_file)
